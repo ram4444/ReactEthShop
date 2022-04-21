f63c6beff8dcb950bc02d1311856f64c90621eac
ram4444 <ramwt4444@gmail.com>
2022-04-22 02:06:07 +0800

    Reorder Router

--- src/routes.js
+++ src/routes.js
@@ -10,26 +10,34 @@ import NotFound from './pages/Page404';
 import Register from './pages/Register';
 import Products from './pages/Products';
 import DashboardApp from './pages/DashboardApp';
+import CreateToken from './pages/CreateToken';
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
   return useRoutes([
     {
+      path: '/list',
+      element: <DashboardLayout />,
+      children: [
+        { path: 'user', element: <User /> },
+        { path: 'products', element: <Products /> },
+        { path: 'blog', element: <Blog /> },
+        { path: 'createtoken', element: <CreateToken /> },
+      ],
+    },
+    {
       path: '/dashboard',
       element: <DashboardLayout />,
       children: [
         { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
-        { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> },
       ],
     },
     {
       path: '/',
       element: <LogoOnlyLayout />,
       children: [
-        { path: '/', element: <Navigate to="/dashboard/app" /> },
+        { path: '/', element: <Navigate to="/list/blog" /> },
         { path: 'login', element: <Login /> },
         { path: 'register', element: <Register /> },
         { path: '404', element: <NotFound /> },
