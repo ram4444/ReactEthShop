41e090c0a3b5909095e59a2c3e8b6921ba1b2052
ram4444 <ramwt4444@gmail.com>
2022-04-22 20:28:47 +0800

    Fix for home button

--- src/layouts/dashboard/NavConfig.js
+++ src/layouts/dashboard/NavConfig.js
@@ -8,7 +8,7 @@ const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
 const navConfig = [
   {
     title: 'home',
-    path: '/list/blog',
+    path: '/list/articles',
     icon: getIcon('eva:file-text-fill'),
   },
   {
