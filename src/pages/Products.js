a7f243b24aedffc8a6e8ba126654bec8d252e61a
ram4444 <ramwt4444@gmail.com>
2022-04-25 01:41:09 +0800

    Bug fix for drupal api JSON call for 2 times

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,110 +1,149 @@
-import React, { useState, useEffect, useContext } from 'react';
-// material
-import { Container, Stack, Typography } from '@mui/material';
+  import React, { useState, useEffect, useContext } from 'react';
+  // material
+  import { Container, Stack, Typography } from '@mui/material';
 
-import axios from 'axios';
-// components
-import Page from '../components/Page';
-import ConnectMetaMask from '../components/ConnectMetaMask';
-import TransferERC777 from '../components/TransferERC777';
-import { ProductSort, ProductList, ProductCartWidget, ProductFilterSidebar } from '../sections/@dashboard/products';
-// mock
-// import PRODUCTS from '../_mock/products';
+  import axios from 'axios';
+  // components
+  import Page from '../components/Page';
+  import ConnectMetaMask from '../components/ConnectMetaMask';
+  import TransferERC777 from '../components/TransferERC777';
+  import { ProductSort, ProductList, ProductCartWidget, ProductFilterSidebar } from '../sections/@dashboard/products';
+  // mock
+  // import PRODUCTS from '../_mock/products';
 
-import { TestContext, ProdContext } from '../Context';
+  import { TestContext, ProdContext } from '../Context';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-export default function EcommerceShop() {
-  const [openFilter, setOpenFilter] = useState(false);
-  const [isLoading, setLoading] = useState(true);
-  const [netId, setNetId] = useState('UNKNOWN');
-  const [pd, setPd] = useState();
+  export default function EcommerceShop() {
+    const [openFilter, setOpenFilter] = useState(false);
+    const [isLoading, setLoading] = useState(true);
+    const [netId, setNetId] = useState('UNKNOWN');
+    const [pd, setPd] = useState();
 
-  const context = useContext(TestContext);
-  const { drupalHostname, localNetId, erc777ContractAddr, receiverAddr } = context;
+    const context = useContext(TestContext);
+    const { drupalHostname, localNetId, erc777ContractAddr, receiverAddr } = context;
 
-
-  function promiseHttp() {
-    return axios({
-      method: 'get',
-      url: `http://${drupalHostname}/jsonapi/node/product?include=field_product_photo`,
-      responseType: 'json',
-      // crossDomain: true,
-      headers: { 'Access-Control-Allow-Origin': '*' }
-    })
-      .then((response) => {
-        console.log('HTTP call done');
-        return response.data;
+    function secondAxio(product) {
+      const prom = axios({
+        method: 'get',
+        url: `http://${drupalHostname}/jsonapi/node/product/${product.id}?include=field_product_photo`,
+        responseType: 'json',
+        // crossDomain: true,
+        headers: { 'Access-Control-Allow-Origin': '*' }
+      }) 
+        .then((responsePhoto) => {
+          console.log('HTTP call for Product photo done');
+          return responsePhoto.data.included[0].attributes.name;
       })
-      .then((data) => {
-        const dataArray = data.data.map((_) => _);
-        const includedArray = data.included.map((_) => _.attributes.name);
-
-        return dataArray.map((_, i) => {
-          console.log('Merging JSON');
-          return {
-            id: _.id,
-            cover: `http://${drupalHostname}/sites/default/files/media/Image/productPhoto/${includedArray[i]}`,
-            name: _.attributes.title,
+        .then((filename) => ({
+            id: product.id,
+            cover: `http://${drupalHostname}/sites/default/files/media/Image/productPhoto/${filename}`,
+            name: product.attributes.title,
             // price: _.attributes.price,
-            price: _.attributes.field_price,
+            price: product.attributes.field_price,
             priceSale: null,
             colors: ['#000000'],
             status: '',
-            receiverAddr: _.attributes.field_walletaddr,
-          };
+            receiverAddr: product.attributes.field_walletaddr,
+            currency: product.attributes.field_currency
+      }))
+      return prom;
+    } 
+
+    function promiseHttp() {
+      return axios({
+        method: 'get',
+        url: `http://${drupalHostname}/jsonapi/node/product`,
+        responseType: 'json',
+        // crossDomain: true,
+        headers: { 'Access-Control-Allow-Origin': '*' }
+      })
+        .then((response) => {
+          console.log('HTTP call for Product lists done');
+          return response.data;
+      })
+        .then((data) => {
+          console.log('----data----');
+          console.log(data);
+          const dataArray = data.data.map((_) => _);
+          return dataArray;
+      })
+        
+    }
+
+    useEffect(() => { 
+      
+        const a = promiseHttp().then((prom) => {
+          // console.log('prom')
+          // console.log(prom)
+          const arr = [];
+          
+          let donecount =0;
+          const s = prom.map((product,i ) => {
+            secondAxio(product).then((prom2) => {
+              // console.log('prom2');
+              // console.log(prom2);
+              arr.push(prom2);
+              donecount+=1;
+              return prom2;
+            }).then((prom2) => {
+              if (donecount===prom.length) {
+                console.log('end')
+                setPd(arr);
+                setLoading(false);
+              }
+              
+            })
+            return[]
+          })
+          return arr;
         });
-      });
-  }
+        
+        console.log(a);
+        
+    }, []);
 
-  useEffect(() => {
-    promiseHttp().then((prom) => {
-      setPd(prom);
-      setLoading(false);
-    });
-  }, []);
+    const handleOpenFilter = () => {
+      setOpenFilter(true);
+    };
 
-  const handleOpenFilter = () => {
-    setOpenFilter(true);
-  };
+    const handleCloseFilter = () => {
+      setOpenFilter(false);
+    };
 
-  const handleCloseFilter = () => {
-    setOpenFilter(false);
-  };
+    const handleSetNetId = (fromChild) => {
+      console.log(fromChild);
+      setNetId(fromChild);
+    };
 
-  const handleSetNetId = (fromChild) => {
-    console.log(fromChild);
-    setNetId(fromChild);
-  };
+    if (isLoading) {
+      return <Page title="LOADING" />;
+    }
 
-  if (isLoading) {
-    return <Page title="LOADING" />;
-  }
-
-  return (
-    <Page title="Dashboard: Products">
-      <Container>
-        <Stack direction="row" spacing={2}>
-          <Typography variant="h4" sx={{ mb: 5 }}>
-            Products
-          </Typography>
-        </Stack>
-
-        <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
-          <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
-            <ProductFilterSidebar
-              isOpenFilter={openFilter}
-              onOpenFilter={handleOpenFilter}
-              onCloseFilter={handleCloseFilter}
-            />
-            <ProductSort />
+    return (
+      <Page title="Dashboard: Products">
+        <Container>
+          <Stack direction="row" spacing={2}>
+            <Typography variant="h4" sx={{ mb: 5 }}>
+              Products
+            </Typography>
           </Stack>
-        </Stack>
 
-        <ProductList products={pd} />
-        <ProductCartWidget />
-      </Container>
-    </Page>
-  );
-}
+          <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
+            <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
+              <ProductFilterSidebar
+                isOpenFilter={openFilter}
+                onOpenFilter={handleOpenFilter}
+                onCloseFilter={handleCloseFilter}
+              />
+              <ProductSort />
+            </Stack>
+          </Stack>
+
+          <ProductList products={pd} />
+          <ProductCartWidget />
+        </Container>
+      </Page>
+    );
+  }
