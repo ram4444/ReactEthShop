c63fe325515c78b31722daf43c756bf42b536a0f
ram4444 <ramwt4444@gmail.com>
2022-04-25 23:28:03 +0800

    Add Etherscan API URL

--- src/properties/urls.js
+++ src/properties/urls.js
@@ -1,4 +1,7 @@
 export const urls = {
   truffleMigrate: 'http://ethshopdemo.dionysbiz.xyz:8081/pb/trufflemigrate',
-  appendwalletaddr: 'http://ethshopdemo.dionysbiz.xyz:8082/backend/appendwalletaddr'
+  appendwalletaddr: 'http://ethshopdemo.dionysbiz.xyz:8082/backend/appendwalletaddr',
+  etherscan_mainnet: 'https://api.etherscan.io/api?module=contract&action=getabi&address=',
+  etherscan_rinkeby: 'https://api-rinkeby.etherscan.io/api?module=contract&action=getabi&address=',
+  etherscan_ropsten: 'https://api-ropsten.etherscan.io/api?module=contract&action=getabi&address='
 };
