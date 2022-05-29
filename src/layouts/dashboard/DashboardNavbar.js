68b2d234e2382113ae3dde9b9594c19a06199113
ram4444 <ramwt4444@gmail.com>
2022-05-29 19:46:46 +0800

    temp Remove multi language

--- src/layouts/dashboard/DashboardNavbar.js
+++ src/layouts/dashboard/DashboardNavbar.js
@@ -61,7 +61,9 @@ export default function DashboardNavbar({ onOpenSidebar }) {
         <Box sx={{ flexGrow: 1 }} />
 
         <Stack direction="row" alignItems="center" spacing={{ xs: 0.5, sm: 1.5 }}>
+          {/*
           <LanguagePopover />
+          */}
           <NotificationsPopover />
           <AccountPopover />
         </Stack>
