24d03a00745a38070771b2826a4b95462d6b8e19
ram4444 <ramwt4444@gmail.com>
2022-06-15 22:39:18 +0800

    Missing commit last time

--- src/sections/@dashboard/home/index.js
+++ src/sections/@dashboard/home/index.js
@@ -2,3 +2,4 @@ export { default as ArticlesPostCard } from './ArticlesPostCard';
 export { default as ArticlesPostsSearch } from './ArticlesPostsSearch';
 export { default as ArticlesPostsSort } from './ArticlesPostSort';
 export { default as UserWidget } from './UserWidget';
+export { default as BannerCard } from './BannerCard';
